eafa5e8dc412a042ad8c28baa76d7264bc87f324
Paul Thanataweenont <paul@krupaul.com>
2026-05-16 22:16:55 -0600

    feat: implement main application structure with routing and components

--- paradise-nursery/src/App.jsx
+++ paradise-nursery/src/App.jsx
@@ -1,2 +1,36 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Head
+import Header from "./components/Header";
+import AboutUs from "./components/AboutUs";
+import ProductList from "./components/ProductList";
+import CartItem from "./components/CartItem";
+import "./App.css";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<AboutUs />} />
+          <Route 
+            path="/plants"
+            element={
+              <>
+                <Header />
+                <ProductList />
+              </>
+            }
+          />          
+          <Route 
+            path="/cart"
+            element={
+              <>
+                <Header />
+                <CartItem />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
